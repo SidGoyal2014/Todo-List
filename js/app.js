2356f34d7af272865998e50fa8ef0947c8964065
Siddharth Goyal <sidgoyal2014@gmail.com>
2021-05-27 22:04:35 +0530

    Add new task
    The task can be inserted now. But, the rest of the functionality is incomplete, including delete task, storing task in cookies, styling etc.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
-function add_new_task(){
+function add_new_task(task_content){
+
+    var block = document.getElementById("tasks");
+
+    var t = document.createElement("p");
+
+    t.innerHTML = task_content;
+
+    block.appendChild(t);
 }
