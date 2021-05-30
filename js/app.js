5070e844139209d14262d44aece2e6eebdc87c6b
Siddharth Goyal <sidgoyal2014@gmail.com>
2021-05-30 16:37:58 +0530

    Functionality complete; design, CSS, bootstrap needs improvement
    The JS functionality is complete. The delete operation is complete.
    The CSS, bootstrap etc, these things need improvement

--- js/app.js
+++ js/app.js
@@ -1,65 +1,40 @@
-// the id not passing
-
-var list_tasks = [];
+var cnt_tasks = 0;
 
 function add_new_task(task_content){
 
-    var block = document.getElementById("tasks");
+    var block = document.getElementById("task_list");
 
+    // Create a <li> list item
+    var li = document.createElement("li");
+    li.id = "task" + cnt_tasks;
+
+    // Create task div & define attributes
     var t = document.createElement("div");
 
-    t.id = "task" + list_tasks.length;
+    // increase the counter
+    cnt_tasks = cnt_tasks+1;
 
-    // Create p & insert values
+    // Create p & define attributes
     var p1 = document.createElement("p");
-    
     p1.innerHTML = task_content;
 
-    /*
-    /// create p & insert values
-
-    var p2 = document.createElement("p");
-    
-    p2.innerHTML = "Delete";
-    */
-
-    // Create delete button & settings
-
+    // Create delete button & define attributes
     var del = document.createElement("button");
-
     del.innerHTML = "Delete";
-
-    // var dele_attribute = "delete_task(" + Number((list_tasks.length)) + ");"
-
-    // var dele_attribute = "delete_task(" + t + ");";
-
-    var dele_attribute = "delete_task(" + (t.id) + ");";
-
-    console.log(t.id);
-
+    var dele_attribute = "delete_task(" + (li.id) + ");";
     del.setAttribute("onclick",dele_attribute);
 
     // Append p1 & del to t
-
     t.appendChild(p1);
     t.appendChild(del);
 
-    // Append t to block
+    // Append t to li
+    li.appendChild(t);
 
-    list_tasks[list_tasks.length] = t;
-
-    block.appendChild(t);
+    // Append li to block
+    block.appendChild(li);
 }
 
-
-// Yet to be d
 function delete_task(num){
-    console.log("You are in delete task function");
-    console.log(String(num));
-    console.log("sdfsdf");
-
-    // var ele = document.getElementById(num);
-
-    // console.log(ele);
     num.remove();
 }
